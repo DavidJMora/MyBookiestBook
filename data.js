31d0b697113a73bf7e687c20f6305c197a43b5ec
David Mora <David.mora@codeimmersives.com>
2019-03-27 14:32:48 -0400

    added all data on data.js page and began writing function for event listeners

--- data.js
+++ data.js
@@ -1,3 +1,252 @@
 const data = [
-    
+    {
+        letter: 'Aa',
+        position: 1,
+        words: [
+            "Artificial amateurs aren't at all amazing",
+            'Analytically, I assault', 
+            'animate things'
+        ]
+    },
+    {
+        letter: 'Bb',
+        position: 2,
+        words: [
+            'Broken barriers bounded by the bomb beat',
+            'Buildings are broken', 
+            "basically I'm bombarding"
+        ]
+    },
+    {
+        letter: 'Cc',
+        position: 3,
+        words: [
+            'Casually create catastrophes',
+            'casualties',
+            'Canceling cats', 
+            'got they canopies collapsing'
+        ]
+    },
+    {
+        letter: 'Dd',
+        position: 4,
+        words: [
+            "Detonate a dime of dank daily doin' dough",
+            'Demonstrations', 
+            'Don Dada on the down low'
+        ]
+    },
+    {
+        letter: 'Ee',
+        position: 5,
+        words: [
+            'Eating other editors with each and every energetic Epileptic episode',
+            'elevated etiquette'
+        ]
+    },
+    {
+        letter: 'Ff',
+        position: 6,
+        words: [
+            'Furious', 
+            'fat', 
+            'fabulous', 
+            'fantastic',
+            'Flurries of funk felt', 
+            'feeding the fanatics'
+        ]
+    },
+    {
+        letter: 'Gg',
+        position: 7,
+        words: [
+            'Gift got great', 
+            'global goods gone glorious',
+            "Gettin' godly in this game with the goriest"
+        ]
+    },
+    {
+        letter: 'Hh',
+        position: 8,
+        words: [
+            "Hit 'em high", 
+            "hella hype", 
+            'historical',
+            'hey holocaust hymns', 
+            "hear 'em holler at your homeboy"
+        ]
+    },
+    {
+        letter: 'Ii',
+        position: 9,
+        words: [
+            'Imitators idolize', 
+            'I intimidate',
+            'In an instant', 
+            "I'll rise in an irate state"
+        ]
+    },
+    {
+        letter: 'Jj',
+        position: 10,
+        words: [
+            'Juiced on my jams like Jheri curls jocking joints',
+            'Justly', 
+            "it's just me", 
+            'writing my journals'
+        ]
+    },
+    {
+        letter: 'Kk',
+        position: 11,
+        words: [
+           " Kindly I'm kindling all kinds of ink on",
+            'Karate kick type Brits in my kingdom'
+        ]
+    },
+    {
+        letter: 'Ll',
+        position: 12,
+        words: [
+            'Let me live a long life', 
+            'lyrically lessons is learned',
+            'lame louses just lose to my livery'
+        ]
+    },
+    {
+        letter: 'Mm',
+        position: 13,
+        words: [
+            'My mind makes marvelous moves masses marvel and move', 
+            "many mock what I've mastered"
+        ]
+    },
+    {
+        letter: 'Nn',
+        position: 14,
+        words: [
+            "Niggas nap knowing I'm nice naturally",
+            'knack never lack', 
+            'make noise nationally'
+        ]
+    },
+    {
+        letter: 'Oo',
+        position: 15,
+        words: [
+            'Operation', 
+            'opposition', 
+            'off', 
+            'not optional',
+            'Out of sight', 
+            'out of mind', 
+            'wide beaming opticals'
+        ]
+    },
+    {
+        letter: 'Pp',
+        position: 16,
+        words: [
+            'Perfected poem', 
+            'powerful punchlines',
+            'Pummeling petty powder puffs in my prime'
+        ]
+    },
+    {
+        letter: 'Qq',
+        position: 17,
+        words: [
+            "Quite quaint quotes keep quiet it's Quannnum",
+            "Quarrelers ain't got a quarter of what we got, uh"
+        ]
+    },
+    {
+        lette: 'Rr',
+        position: 18,
+        words: [
+            'Really raw raps', 
+            "risin' up rapidly",
+            'Riding the rushing radioactivity'
+        ]
+    },
+    {
+        letter: 'Ss',
+        position: 19,
+        words: [
+            'Super scientifical',
+            'sound',
+            'search',
+            'sought',
+            'Silencing super fire saps that are soft'
+        ]
+    },
+    {
+        letter: 'Tt',
+        position: 20,
+        words: [
+            'Tales ten times talented', 
+            'too tough',
+            'take that challengers, get a tune up'
+        ]
+    },
+    {
+        letter: 'Uu',
+        position: 21,
+        words: [
+            'Universal', 
+            'unique', 
+            'untouched',
+            'Unadulterated, the raw uncut'
+        ]
+    },
+    {
+        letter: 'Vv',
+        position: 22,
+        words: [
+            'Verb', 
+            'vice lord', 
+            'victorious',
+            'valid',
+            "Violate vibes that are vain make 'em vanish"
+        ]
+    },
+    {
+        letter: 'Ww',
+        position: 23,
+        words: [
+            "While I'm all well would a wise wordsmith just weaving up words", 
+            'weeded up on my workshift'
+        ]
+    },
+    {
+        letter: 'Xx',
+        position: 24,
+        words: [
+            "Xerox my X-radiation holes",
+            'extra large',
+            'X-height letters and xylophone tones'
+        ]
+    },
+    {
+        letter: 'Yy',
+        position: 25,
+        words: [
+            'Yellow-back', 
+            'yak mouth', 
+            "young ones' yaws",
+            "Yesterday's lawn yardsale, I yawn"
+        ]
+    },
+    {
+        letter: 'Zz',
+        position: 26,
+        words: [
+            'Zig-Zag zombies', 
+            'zooming to the zenith',
+            'Zero in', 
+            'Zen thoughts', 
+            'overzealous rhyme',
+            'ZEA-LOTS!'
+        ]
+    }
 ]
--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 window.onload = init;
+
+function init () {
+    const searchButton = document.querySelector('#search');
+    searchButton.addEventListener('click',);
+
+    const displayAllButton = document.querySelector('#display-all');
+    displayAllButton.addEventListener('click')
+}
